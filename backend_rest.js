88d9d5510bcf51258cda310ab64c866473eb6b4e
mikeyu4281 <mikeyu4281@gmail.com>
2020-03-30 18:17:57 -0400

    Update backend_rest.js

--- backend_rest.js
+++ backend_rest.js
@@ -54,8 +54,8 @@ const port = process.env.PORT || 5000;
 const db = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
-  password : 'courtpiece',
-  database : 'chats'
+  password : 'test',
+  database : 'photodb'
 });
 
 var registeruserSchema = mongoose.Schema(
@@ -71,4 +71,60 @@ var registeruserSchema = mongoose.Schema(
 db.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+});
+
+
+app.post('/api/upload', upload.single('img'), function (req, res) {
+    
+	
+	console.log("req in",req.body.imageObject.img);
+	var dateTime = new Date(timeInMs);
+	let upload = multer({ storage: storage, fileFilter: helpers.imageFilter }).single('img');
+	console.log("req",req);
+	
+	var imageData = fs.readFileSync(req.file);
+	
+		console.log("insert file...........................................", imageData);
+	
+	
+	console.log(req.body.imageObject.img);
+	
+	 if (req.body.imageObject.img) {
+        console.log('Uploading file...');
+        var filename = req.body.imageObject.img;
+        var uploadStatus = 'File Uploaded Successfully';
+    } else {
+        console.log('No File Uploaded');
+        var filename = 'FILE NOT UPLOADED';
+        var uploadStatus = 'File Upload Failed';
+    }
+	
+	console.log("After file check!!!!!!!!!!!!");
+	
+	 upload(req, res, function(err) {
+       
+
+
+    });
+	
+	if (!req.file) {
+        console.log("No file received");
+        return res.send({
+          success: false
+        });
+    
+      } else {
+		  
+		 upload(req,res,function(err) {
+       
+			if(err) {
+				return res.end(errJson);
+			}
+			res.end("File is uploaded");
+		});
+        console.log('file received    ' + dateTime);
+        return res.send({
+          success: true
+        })
+      }
 });
