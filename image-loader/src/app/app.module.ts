61937102bf657051806c825898bfc8491eb0e375
Elias J Manzueta <eliasmanz@gmail.com>
2020-03-31 00:53:21 -0400

    add new component with routing

--- image-loader/src/app/app.module.ts
+++ image-loader/src/app/app.module.ts
@@ -1,14 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ImageDisplayComponent } from './image-display/image-display.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'image-display',
+    component: ImageDisplayComponent,
+    data: { title: 'Images' }
+  },
+  { path: '',
+    redirectTo: '/image-display',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImageDisplayComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { enableTracing: true })
   ],
   providers: [],
   bootstrap: [AppComponent]
